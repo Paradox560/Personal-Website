a64970677812d9b3a5994521595e5aec3548f3d7
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-07-28 22:38:48 -0400

    modified email submission

--- forms/contact.php
+++ forms/contact.php
@@ -6,36 +6,25 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  if (isset($_POST['submit'])) {
+    $to = "pranav4096@gmail.com"; // Your email address
+    $name = $_POST['name'];
+    $from = $_POST['email'];
+    $message = $_POST['message'];
+    $subject = $name . " - Contact Form Submission";
+    $headers = "From:" . $from;
+    $result = mail($to, $subject, $message, $headers);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    if ($result)
+    {
+        echo '<script type="text/javascript">alert("Your message was sent!");</script>';
+        echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+
+    }
+    else
+    {
+        echo '<script type="text/javascript">alert("Oops! Your message wasn’t sent. Try again later.");</script>';
+        echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+    }
   }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
